a59a5049578404927b139c419403937860e31d6a
Your Name <you@example.com>
2023-04-04 10:36:44 +0530

    node

---
+++ data.js
@@ -0,0 +1,10 @@
+// const data = { name: 'Aniket', email: 'aniket@gmail.com' };
+
+const data = [
+    { name: 'Aniket', email: 'aniket@gmail.com',contact:"23432432" },
+    { name: 'Saaam', email: 'Saaam@gmail.com',contact:"23432432" },
+    { name: 'Prooo', email: 'Prooo@gmail.com',contact:"23432432" },
+    { name: 'Jioioio', email: 'Jioioio@gmail.com',contact:"23432432" },
+    { name: 'Zxxxx', email: 'Zxxxxx@gmail.com',contact:"23432432" }
+]
+module.exports = data;
